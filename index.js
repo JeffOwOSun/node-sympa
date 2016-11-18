ef68b07d23898a598d2e8136dc495a1e345ebf32
Yushi Sun <sunyushi9546@gmail.com>
2016-11-18 22:33:34 +0800

    cannot figure out why the auth is not working. swithing to phantom js

--- index.js
+++ index.js
@@ -1,49 +1,82 @@
-var rp = require("request-promise").defaults({jar: true});
+var j = require("request").jar();
+var rp = require("request-promise").defaults({jar: j});
 var cheerio = require("cheerio");
 var settings = require('./settings.js');
 
-var cookie, ticket, execution, recirectedUrl, loginMsg;
-
-rp({
-  method: 'POST',
-  uri: settings.sympaRoot,
-  resolveWithFullResponse: true,
-  form: {
-    action: 'sso_login',
-    auth_service_name: 'Login',
-    action_sso_login: 'Login',
-  },
-}).then(function(res) {
+var SYMPA = module.exports = function SYMPA(sympaRoot) {
+  this.sympaRoot = sympaRoot;
+}
+//get cookies
+SYMPA.prototype.setup = function (username, password, casRoot) {
+  var self = this;
   return rp({
-    method: 'GET',
-    uri: settings.loginUrl,
-    resolveWithFullResponse: true,
-  });
-})
-.then(function(response) {
-  var $ = cheerio.load(response.body);
-  ticket = $('input[name=lt]').attr('value');
-  execution = $('input[name=execution]').attr('value');
-
-  loginMsg = {
-    'username': settings.username,
-    'password': settings.password,
-    'lt': ticket,
-    'execution': execution,
-    '_eventId': 'submit',
-    'submit': 'LOGIN',
-  }
-  console.log(loginMsg);
-  return rp({
+    //click the login button in sympa
     method: 'POST',
-    uri: settings.loginUrl,
+    uri: self.sympaRoot,
     resolveWithFullResponse: true,
-    form: loginMsg,
+    simple: false,
+    form: {
+      action: 'sso_login',
+      auth_service_name: 'Login',
+      action_sso_login: 'Login',
+    },
+  }).then(function(res) {
+    //get the tickets
+    return rp({
+      method: 'GET',
+      uri: casRoot+'/login?service='+self.sympaRoot+'/sso_login_succeeded/Login',
+      resolveWithFullResponse: true,
+    });
+  })
+  .then(function(response) {
+    //console.log(j);
+    var jsessionId = j.getCookies(casRoot)[0].value;
+    //login to cas
+    //console.log(j.serializeSync());
+    var $ = cheerio.load(response.body);
+    var ticket = $('input[name=lt]').attr('value');
+    var execution = $('input[name=execution]').attr('value');
+    var loginMsg = {
+      'username': username,
+      'password': password,
+      'lt': ticket,
+      'execution': execution,
+      '_eventId': 'submit',
+      'submit': 'LOGIN',
+    }
+    //console.log(loginMsg);
+    return rp({
+      method: 'POST',
+      uri: casRoot+'/login;jsessionid='+jsessionId+'?service='+self.sympaRoot+'/sso_login_succeeded/Login',
+      resolveWithFullResponse: true,
+      form: loginMsg,
+      simple: false,
+    });
+  })
+  .then(function(response) {
+    console.log('success', response);
+  })
+  .catch(function(err) {
+    throw err;
   });
-})
-.then(function(response) {
-  console.log('success', response);
+}
+SYMPA.prototype.getList = function (mailingList) {
+  return rp({
+    uri: this.sympaRoot+'/dump/'+mailingList+'/light',
+  })
+  //.then(function(body) {
+  //  return body.split('\n');
+  //});
+}
+
+var sympa = new SYMPA(settings.sympaRoot);
+sympa.setup(settings.username, settings.password, settings.casRoot)
+//.then(function() {
+//  return sympa.getList('jowos-custom-list');
+//})
+.then(function(res) {
+  console.log(res);
 })
 .catch(function(err) {
   console.error(err);
-});
+})
